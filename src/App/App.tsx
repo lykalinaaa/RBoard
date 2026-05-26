203253f3d07e83e69fad5dab1e71902bd062594c
alina <alina.lukianchenkova@tages.ru>
2026-05-26 15:35:59 +0300

    RBoard-31: Настройка маршрутизации

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,19 @@
+import * as React from 'react';
+import MainPage from '@/Pages/MainPage/ui/MainPage';
+import Auth from '@/Pages/Auth/ui/Auth';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
